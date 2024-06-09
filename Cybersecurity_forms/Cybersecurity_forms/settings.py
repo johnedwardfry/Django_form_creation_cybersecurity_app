1ce89607f6d49918a0f4a894d79a43c1c5a6e3f1
johnedwardfry <johnedwardfry@gmail.com>
2024-06-09 13:41:32 -0700

    Updated the files and added the app tothe installed apps list
    these will need to be returned so that they will be displayed in the template.

--- Cybersecurity_forms/Cybersecurity_forms/settings.py
+++ Cybersecurity_forms/Cybersecurity_forms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.Cybersecurity_forms.Risk_register'
 ]
 
 MIDDLEWARE = [
--- Cybersecurity_forms/Risk_register/models.py
+++ Cybersecurity_forms/Risk_register/models.py
@@ -1,3 +1,32 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+
 # Create your models here.
+
+class RISK_MATRISK(models.Model):
+    date_raised = models.DateField()
+    RISK_NAME = models.CharField(max_length=200)
+    RISK_OWNER = models.CharField(max_length=200)
+    RISK_OWNER_EMAIL = models.CharField(max_length=200)
+    RISK_DESCRIPTION = models.TextField()
+    MITIGATING_ACTION = models.TextField()
+
+    RISK_LIKELIHOOD = models.IntegerField(default=1,
+                                          validators=[
+                                              MaxValueValidator(3),
+                                              MinValueValidator(1)
+                                          ])
+
+    RISK_IMPACT = models.IntegerField(default=1,
+                                      validators=[
+                                          MaxValueValidator(3),
+                                          MinValueValidator(1)
+                                      ])
+
+    RISK_SEVERITY = models.IntegerField(editable=False, )
+
+    def save(self, *args, **kwargs):
+        # Calculate RISK_SEVERITY
+        self.RISK_SEVERITY = self.RISK_LIKELIHOOD * self.RISK_IMPACT
+        super().save(*args, **kwargs)
