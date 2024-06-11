740edd0a3dd15f8743f329d098e44783135faaef
johnedwardfry <johnedwardfry@gmail.com>
2024-06-10 21:21:10 -0700

    fixed port issues and added a migration

--- init.sh
+++ init.sh
@@ -3,10 +3,11 @@
 
 # Migrate the database
 python Cybersecurity_forms/manage.py makemigrations
+
 python Cybersecurity_forms/manage.py migrate
 # Collect static files
 python Cybersecurity_forms/manage.py collectstatic --noinput
 # Create a superuser
 python Cybersecurity_forms/manage.py createsuperuser --noinput
 # Start the server
-python Cybersecurity_forms/manage.py runserver 0.0.0.0:8000
+python Cybersecurity_forms/manage.py runserver 0.0.0.0:8001
